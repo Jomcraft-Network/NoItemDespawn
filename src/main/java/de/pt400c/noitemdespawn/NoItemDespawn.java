d310125373049dabfb6047a33ac582d351fd50d4
PT400C Jomcraft <pt400c@gmail.com>
2020-06-05 21:37:11 +0200

    Upped version

--- src/main/java/de/pt400c/noitemdespawn/NoItemDespawn.java
+++ src/main/java/de/pt400c/noitemdespawn/NoItemDespawn.java
@@ -17,7 +17,7 @@ public class NoItemDespawn {
  
 	public static final String MODID = "noitemdespawn";
 	public static final Logger log = LogManager.getLogger(NoItemDespawn.MODID);
-	public static final String VERSION = "2.0";
+	public static final String VERSION = "2.0.1";
 	public static NoItemDespawn instance;
 
 	public NoItemDespawn() {
